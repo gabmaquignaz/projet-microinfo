eccb36e9933a9a1f2a36bfa4ba2ffcdca2319873
gabmaquignaz <61877971+gabmaquignaz@users.noreply.github.com>
2020-03-12 09:22:06 +0100

    Add TP3

---
+++ TP3_Chibi/Chibi/.dep/main.o.d
@@ -0,0 +1,364 @@
+build/obj/main.o: main.c ../../lib/ChibiOS/os/rt/include/ch.h \
+ ../../lib/ChibiOS/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stddef.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/stdint.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/features.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h \
+ chconf.h ../../lib/ChibiOS/os/rt/include/chlicense.h \
+ ../../lib/ChibiOS/os/rt/include/chsystypes.h \
+ ../../lib/ChibiOS/os/rt/ports/ARMCMx/chcore.h \
+ ../../lib/ChibiOS/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h \
+ epuck2_board/board.h ../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f4xx.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f407xx.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cmInstr.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cmFunc.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4_simd.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/ST/system_stm32f4xx.h \
+ ../../lib/ChibiOS/os/rt/ports/ARMCMx/chcore_v7m.h \
+ ../../lib/ChibiOS/os/rt/include/chdebug.h \
+ ../../lib/ChibiOS/os/rt/include/chtm.h \
+ ../../lib/ChibiOS/os/rt/include/chstats.h \
+ ../../lib/ChibiOS/os/rt/include/chschd.h \
+ ../../lib/ChibiOS/os/rt/include/chsys.h \
+ ../../lib/ChibiOS/os/rt/include/chvt.h \
+ ../../lib/ChibiOS/os/rt/include/chthreads.h \
+ ../../lib/ChibiOS/os/rt/include/chregistry.h \
+ ../../lib/ChibiOS/os/rt/include/chsem.h \
+ ../../lib/ChibiOS/os/rt/include/chbsem.h \
+ ../../lib/ChibiOS/os/rt/include/chmtx.h \
+ ../../lib/ChibiOS/os/rt/include/chcond.h \
+ ../../lib/ChibiOS/os/rt/include/chevents.h \
+ ../../lib/ChibiOS/os/rt/include/chmsg.h \
+ ../../lib/ChibiOS/os/rt/include/chmboxes.h \
+ ../../lib/ChibiOS/os/rt/include/chmemcore.h \
+ ../../lib/ChibiOS/os/rt/include/chheap.h \
+ ../../lib/ChibiOS/os/rt/include/chmempools.h \
+ ../../lib/ChibiOS/os/rt/include/chdynamic.h \
+ ../../lib/ChibiOS/os/rt/include/chqueues.h \
+ ../../lib/ChibiOS/os/rt/include/chstreams.h \
+ ../../lib/ChibiOS/os/hal/include/hal.h \
+ ../../lib/ChibiOS/os/hal/osal/rt/osal.h halconf.h mcuconf.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/hal_lld.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_registry.h \
+ ../../lib/ChibiOS/os/hal/ports/common/ARMCMx/nvic.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_isr.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_dma.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_rcc.h \
+ ../../lib/ChibiOS/os/hal/include/hal_streams.h \
+ ../../lib/ChibiOS/os/hal/include/hal_channels.h \
+ ../../lib/ChibiOS/os/hal/include/hal_files.h \
+ ../../lib/ChibiOS/os/hal/include/hal_ioblock.h \
+ ../../lib/ChibiOS/os/hal/include/hal_mmcsd.h \
+ ../../lib/ChibiOS/os/hal/include/hal_queues.h \
+ ../../lib/ChibiOS/os/hal/include/pal.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/GPIOv2/pal_lld.h \
+ ../../lib/ChibiOS/os/hal/include/adc.h \
+ ../../lib/ChibiOS/os/hal/include/can.h \
+ ../../lib/ChibiOS/os/hal/include/dac.h \
+ ../../lib/ChibiOS/os/hal/include/ext.h \
+ ../../lib/ChibiOS/os/hal/include/gpt.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/gpt_lld.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h \
+ ../../lib/ChibiOS/os/hal/include/i2c.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/I2Cv1/i2c_lld.h \
+ ../../lib/ChibiOS/os/hal/include/i2s.h \
+ ../../lib/ChibiOS/os/hal/include/icu.h \
+ ../../lib/ChibiOS/os/hal/include/mac.h \
+ ../../lib/ChibiOS/os/hal/include/mii.h \
+ ../../lib/ChibiOS/os/hal/include/pwm.h \
+ ../../lib/ChibiOS/os/hal/include/rtc.h \
+ ../../lib/ChibiOS/os/hal/include/serial.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/USARTv1/serial_lld.h \
+ ../../lib/ChibiOS/os/hal/include/sdc.h \
+ ../../lib/ChibiOS/os/hal/include/spi.h \
+ ../../lib/ChibiOS/os/hal/include/uart.h \
+ ../../lib/ChibiOS/os/hal/include/usb.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/OTGv1/usb_lld.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/OTGv1/stm32_otg.h \
+ ../../lib/ChibiOS/os/hal/include/st.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/st_lld.h mcuconf.h \
+ ../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_registry.h \
+ ../../lib/ChibiOS/os/hal/include/mmc_spi.h \
+ ../../lib/ChibiOS/os/hal/include/serial_usb.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/math.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/reent.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_ansi.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/newlib.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/config.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/ieeefp.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_types.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_types.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/lock.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/cdefs.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_ansi.h \
+ ../../lib/ChibiOS/os/hal/lib/streams/chprintf.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdarg.h \
+ ../../lib/msgbus/messagebus.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/unistd.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/unistd.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/types.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/endian.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_endian.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/select.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_sigset.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_timeval.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/timespec.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_timespec.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/sched.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/types.h \
+ i2c_bus.h imu.h mpu9250.h mpu9250_registers.h
+
+../../lib/ChibiOS/os/rt/include/ch.h:
+
+../../lib/ChibiOS/os/rt/ports/ARMCMx/compilers/GCC/chtypes.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stddef.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/stdint.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/features.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_stdint.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h:
+
+chconf.h:
+
+../../lib/ChibiOS/os/rt/include/chlicense.h:
+
+../../lib/ChibiOS/os/rt/include/chsystypes.h:
+
+../../lib/ChibiOS/os/rt/ports/ARMCMx/chcore.h:
+
+../../lib/ChibiOS/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h:
+
+epuck2_board/board.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f4xx.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f407xx.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cmInstr.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cmFunc.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4_simd.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/system_stm32f4xx.h:
+
+../../lib/ChibiOS/os/rt/ports/ARMCMx/chcore_v7m.h:
+
+../../lib/ChibiOS/os/rt/include/chdebug.h:
+
+../../lib/ChibiOS/os/rt/include/chtm.h:
+
+../../lib/ChibiOS/os/rt/include/chstats.h:
+
+../../lib/ChibiOS/os/rt/include/chschd.h:
+
+../../lib/ChibiOS/os/rt/include/chsys.h:
+
+../../lib/ChibiOS/os/rt/include/chvt.h:
+
+../../lib/ChibiOS/os/rt/include/chthreads.h:
+
+../../lib/ChibiOS/os/rt/include/chregistry.h:
+
+../../lib/ChibiOS/os/rt/include/chsem.h:
+
+../../lib/ChibiOS/os/rt/include/chbsem.h:
+
+../../lib/ChibiOS/os/rt/include/chmtx.h:
+
+../../lib/ChibiOS/os/rt/include/chcond.h:
+
+../../lib/ChibiOS/os/rt/include/chevents.h:
+
+../../lib/ChibiOS/os/rt/include/chmsg.h:
+
+../../lib/ChibiOS/os/rt/include/chmboxes.h:
+
+../../lib/ChibiOS/os/rt/include/chmemcore.h:
+
+../../lib/ChibiOS/os/rt/include/chheap.h:
+
+../../lib/ChibiOS/os/rt/include/chmempools.h:
+
+../../lib/ChibiOS/os/rt/include/chdynamic.h:
+
+../../lib/ChibiOS/os/rt/include/chqueues.h:
+
+../../lib/ChibiOS/os/rt/include/chstreams.h:
+
+../../lib/ChibiOS/os/hal/include/hal.h:
+
+../../lib/ChibiOS/os/hal/osal/rt/osal.h:
+
+halconf.h:
+
+mcuconf.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/hal_lld.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_registry.h:
+
+../../lib/ChibiOS/os/hal/ports/common/ARMCMx/nvic.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_isr.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_dma.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_rcc.h:
+
+../../lib/ChibiOS/os/hal/include/hal_streams.h:
+
+../../lib/ChibiOS/os/hal/include/hal_channels.h:
+
+../../lib/ChibiOS/os/hal/include/hal_files.h:
+
+../../lib/ChibiOS/os/hal/include/hal_ioblock.h:
+
+../../lib/ChibiOS/os/hal/include/hal_mmcsd.h:
+
+../../lib/ChibiOS/os/hal/include/hal_queues.h:
+
+../../lib/ChibiOS/os/hal/include/pal.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/GPIOv2/pal_lld.h:
+
+../../lib/ChibiOS/os/hal/include/adc.h:
+
+../../lib/ChibiOS/os/hal/include/can.h:
+
+../../lib/ChibiOS/os/hal/include/dac.h:
+
+../../lib/ChibiOS/os/hal/include/ext.h:
+
+../../lib/ChibiOS/os/hal/include/gpt.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/gpt_lld.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/stm32_tim.h:
+
+../../lib/ChibiOS/os/hal/include/i2c.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/I2Cv1/i2c_lld.h:
+
+../../lib/ChibiOS/os/hal/include/i2s.h:
+
+../../lib/ChibiOS/os/hal/include/icu.h:
+
+../../lib/ChibiOS/os/hal/include/mac.h:
+
+../../lib/ChibiOS/os/hal/include/mii.h:
+
+../../lib/ChibiOS/os/hal/include/pwm.h:
+
+../../lib/ChibiOS/os/hal/include/rtc.h:
+
+../../lib/ChibiOS/os/hal/include/serial.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/USARTv1/serial_lld.h:
+
+../../lib/ChibiOS/os/hal/include/sdc.h:
+
+../../lib/ChibiOS/os/hal/include/spi.h:
+
+../../lib/ChibiOS/os/hal/include/uart.h:
+
+../../lib/ChibiOS/os/hal/include/usb.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/OTGv1/usb_lld.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/OTGv1/stm32_otg.h:
+
+../../lib/ChibiOS/os/hal/include/st.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/LLD/TIMv1/st_lld.h:
+
+mcuconf.h:
+
+../../lib/ChibiOS/os/hal/ports/STM32/STM32F4xx/stm32_registry.h:
+
+../../lib/ChibiOS/os/hal/include/mmc_spi.h:
+
+../../lib/ChibiOS/os/hal/include/serial_usb.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/math.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/reent.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_ansi.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/newlib.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/config.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/ieeefp.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_types.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_types.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/lock.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/cdefs.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_ansi.h:
+
+../../lib/ChibiOS/os/hal/lib/streams/chprintf.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdarg.h:
+
+../../lib/msgbus/messagebus.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/unistd.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/unistd.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/types.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/endian.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_endian.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/select.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_sigset.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_timeval.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/timespec.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_timespec.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_pthreadtypes.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/sched.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/types.h:
+
+i2c_bus.h:
+
+imu.h:
+
+mpu9250.h:
+
+mpu9250_registers.h:
---
+++ TP3_Chibi/Chibi/.dep/vectors.o.d
@@ -0,0 +1,52 @@
+build/obj/vectors.o: \
+ ../../lib/ChibiOS/os/common/ports/ARMCMx/compilers/GCC/vectors.c \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/stdint.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_default_types.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/features.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_newlib_version.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_intsup.h \
+ /Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_stdint.h \
+ ../../lib/ChibiOS/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h \
+ epuck2_board/board.h ../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f4xx.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f407xx.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cmInstr.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cmFunc.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4_simd.h \
+ ../../lib/ChibiOS/os/ext/CMSIS/ST/system_stm32f4xx.h
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdbool.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/lib/gcc/arm-none-eabi/7.2.1/include/stdint.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/stdint.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/machine/_default_types.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/features.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/_newlib_version.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_intsup.h:
+
+/Applications/Eclipse_e-puck2.app/Contents/Eclipse_e-puck2/Tools/gcc-arm-none-eabi-7-2017-q4-major/arm-none-eabi/include/sys/_stdint.h:
+
+../../lib/ChibiOS/os/common/ports/ARMCMx/devices/STM32F4xx/cmparams.h:
+
+epuck2_board/board.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f4xx.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/stm32f407xx.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cmInstr.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cmFunc.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/include/core_cm4_simd.h:
+
+../../lib/ChibiOS/os/ext/CMSIS/ST/system_stm32f4xx.h:
