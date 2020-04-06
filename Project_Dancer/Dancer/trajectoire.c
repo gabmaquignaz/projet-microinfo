76247189e78f295f20715e5f713c885654ac2df4
Maxime Poffet <maxime.poffet@epfl.ch>
2020-04-06 20:49:04 +0200

    convert function
    convert the angle and the distance

--- Project_Dancer/Dancer/trajectoire.c
+++ Project_Dancer/Dancer/trajectoire.c
@@ -10,11 +10,17 @@
 #include <trajectoire.h>
 
 #define NB_POS		100
+#define ECART		5
+#define RAYON		2
+#define PI			3.1415
+
 
 void convert_pos(void){
 
 	float pos_pol[2*NB_POS] = {0};
 
+	//essais qui marche mais moche
+	/*
 	float r_one = pos_pol[0];
 	float r_two;
 
@@ -26,7 +32,33 @@ void convert_pos(void){
 
 		pos_pol[2*i] = sqrt((r_two*r_two) + (r_one*r_one) + 2*r_two*r_one*cos(pos_pol[2*i+1-2]-pos_pol[2*i+1]));
 
+		pos_pol[2*i+1] =
+
 	}
+	*/
+
+	for(uint8_t i = NB_POS ; i > 1 ; i--){
+
+		float r_mem = pos_pol[2*i];
+
+		//distance between two consecutive points
+		pos_pol[2*i] = sqrt((pos_pol[2*i-2]*pos_pol[2*i-2])
+							+ (pos_pol[2*i]*pos_pol[2*i])
+							+ 2*pos_pol[2*i-2]*pos_pol[2*i]*cos(pos_pol[2*i+1-2]-pos_pol[2*i+1]));
+
+		//angle between two consecutive vectors
+		pos_pol[2*i+1] = PI - (r_mem*sin(pos_pol[2*i+1-2] - pos_pol[2*i+1]))/pos_pol[2*i];
+	}
+
+
+
+	/*
+	for(uint8_t i = 0 ; i < NB_POS ; i++){
+
+
+
+	}
+	*/
 
 }
 
