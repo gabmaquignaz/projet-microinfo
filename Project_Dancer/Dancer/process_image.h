3316cf70be838b2c32a9ead8af0a962758d4b3bd
Maxime Poffet <maxime.poffet@epfl.ch>
2020-04-23 10:29:34 +0200

    delete process_image

--- Project_Dancer/Dancer/process_image.h
+++
@@ -1,9 +0,0 @@
-#ifndef PROCESS_IMAGE_H
-#define PROCESS_IMAGE_H
-
-float get_distance_cm(void);
-float get_hor_pos(void);
-void process_image_start(void);
-void line_position (uint8_t* image, uint16_t size);
-
-#endif /* PROCESS_IMAGE_H */
